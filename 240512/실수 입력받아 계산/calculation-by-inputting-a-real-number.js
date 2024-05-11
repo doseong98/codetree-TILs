47cc6a5c1279f6dfdbe6bf1f992291a00b3f6f5f
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-12 04:01:21 +0900

    Title: 실수 입력받아 계산, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240512/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 240512/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split("\n");
 let a = Number(arr[0]), b = Number(arr[1]);
 let c = a+b;
-console.log(c.toFiex(2));
+console.log(c.toFixed(2));
