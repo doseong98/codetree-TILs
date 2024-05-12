29a78809e90e701134a6386b709e1f9db0f9243c
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-13 02:13:49 +0900

    Title: 정수 세 개 입력받아 출력, Time: 226ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240513/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 240513/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split("\n");
-let a = arr[0], b = arr[1], c = arr[2];
+let a = Number(arr[0]), b = Number(arr[1]), c = Number(arr[2]);
 console.log(a,b);
 console.log(c);
