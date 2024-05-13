9f89ea32cdf91290493706b26362489d98e3ffc1
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-14 02:22:07 +0900

    Title: 두수의 곱과 몫, Time: 245ms, Memory: 15MB, Status: Passed - CodeTree

--- 240514/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240514/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 let arr = fs.readFileSync(0).toString().trim().split(" ");
 let a = arr[0], b = arr[1];
 console.log(`${a} * ${b} = %d`, a*b);
-console.log(`${a} / ${b} = %d`, a/b);
+console.log(`${a} / ${b} = %d`, Math.floor(a/b));
