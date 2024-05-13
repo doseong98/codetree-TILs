cf8f4b325256cc5354ad6326259314bed57ffbae
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-14 02:16:44 +0900

    Title: 간단한 사칙연산, Time: 198ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240514/간단한 사칙연산/simple-arithmetic-operation.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(arr[0]), b = Number(arr[1]);
+console.log(a+b);
+console.log(a-b);
+console.log(Math.floor(a/b));
+console.log(a%b);
