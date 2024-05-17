5341b36935b673aa6803d1d6197477bcd43f191d
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-18 00:11:44 +0900

    Title: 합과 평균, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240518/합과 평균/sum-and-avg.js
+++ 240518/합과 평균/sum-and-avg.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split(" ");
 let a = Number(arr[0]), b = Number(arr[1]);
-let sum = a + b;
-console.log(sum, sum/2);
+let sum = a + b, div = sum/2
+console.log(sum, div.toFixed(1);
