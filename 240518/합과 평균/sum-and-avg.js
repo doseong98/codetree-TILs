78ae9da12210351f53afd5ce648087f3e3bd8c42
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-18 00:12:00 +0900

    Title: 합과 평균, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

--- 240518/합과 평균/sum-and-avg.js
+++ 240518/합과 평균/sum-and-avg.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split(" ");
 let a = Number(arr[0]), b = Number(arr[1]);
 let sum = a + b, div = sum/2
-console.log(sum, div.toFixed(1);
+console.log(sum, div.toFixed(1));
