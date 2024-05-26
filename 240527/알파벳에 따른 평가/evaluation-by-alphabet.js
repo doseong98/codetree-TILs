af88d7986b2b2f5272ba0a0957f275385f4cf21b
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-27 00:25:58 +0900

    Title: 알파벳에 따른 평가, Time: 167ms, Memory: 14MB, Status: Passed - CodeTree

--- 240527/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 240527/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
-let arr = fs.readFileSync(0).toString().trim().split(" ");
+let arr = fs.readFileSync(0).toString().trim();
 
-let a = arr[0];
+let a = arr;
 if (a === "S") {
     console.log("Superior");
 }else if (a === "A") { 
     console.log("Excellent")
+}else if (a === "B") {
+    console.log("Good")
 }else if (a === "C") {
     console.log("Usually")
 }else if (a === "D") {
