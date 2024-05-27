e0c995c0b36a0aa0333e0d6bf9ed5502624a7cd3
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-28 01:45:01 +0900

    Title: 두 숫자의 짝홀 여부, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240528/두 숫자의 짝홀 여부/parity-of-two-numbers.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(arr[0]), b = Number(arr[1]);
+if (a%2 === 0) {
+    console.log("even")
+}else {
+        console.log("odd")
+}
+
+if (b % 2 === 0) {
+    console.log("even")
+}else {
+        console.log("odd")
+}
