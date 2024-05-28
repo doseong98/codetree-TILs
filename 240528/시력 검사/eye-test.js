5b83d2e0e1b697d13a2d0b39167d6041d98329be
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-28 22:55:04 +0900

    Title: 시력 검사, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240528/시력 검사/eye-test.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n")
+let arr = input.join(" ").split(" ")
+
+let a = Number(arr[0]), b = Number(arr[1])
+
+if (a >= 1.0 && b >= 1.0) {
+    console.log("High")
+}else if (a >= 0.5 && b >= 0.5) {
+    console.log("Middle")
+} else console.log("Low")
