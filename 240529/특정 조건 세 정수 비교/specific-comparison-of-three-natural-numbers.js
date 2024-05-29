c313b37440e1e56bca3230bdf568c7cbc923251a
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-29 19:27:05 +0900

    Title: 특정 조건 세 정수 비교, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240529/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240529/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,11 +5,11 @@ let arr = input.join(" ").split(" ")
 let a = Number(arr[0]), b = Number(arr[1]), c = Number(arr[2]);
 let d = 0, e = 0;
 
-if(a < b && a < c) {
+if(a <= b && a <= c) {
     d = 1
 }
 
-if (a == b && b == c) {
+if (a === b && b === c) {
     e = 0
 }
 
