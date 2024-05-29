279f9d57af3661d58bb0d13bf2c19eb3dc2b21c7
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-05-29 20:38:08 +0900

    Title: 좀 더 어려운 수학 점수, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240529/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 240529/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -4,11 +4,9 @@ let arr = input.join(" ").split(" ")
 let Am = Number(arr[0]), Ae = Number(arr[1]), Bm = Number(arr[2]), Be = Number(arr[3]);
 let answer = 0;
 
-if (Am >= Bm) {
+if (Am > Bm) {
     answer = "A"
-}else {
-    answer = "B"
-}
+}else answer = "B"
 
 if (Am === Bm && Ae > Be) {
      answer = "A"
